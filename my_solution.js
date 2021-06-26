85925cfaf87af3c80f00185101a7816709025913
Ashley <ashleywong678@gmail.com>
2021-06-26 14:59:57 -0400

    Create my_solution.js

---
+++ my_solution.js
@@ -0,0 +1,22 @@
+function(s) {
+    
+    let longestLeng = 0
+    for(let i = 0; i < s.length; i++){
+        let subStr = s[i]
+        
+        let repeat = false
+        let j = i + 1
+        while(!repeat && j < s.length){
+            if(subStr.includes(s[j])){
+                repeat = true
+            } else {
+                subStr += s[j]
+                j++
+            }
+        }
+        console.log('sub', subStr)
+        longestLeng = Math.max(longestLeng, subStr.length)
+    }
+
+    return longestLeng
+};
